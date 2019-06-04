206b9798aa961a09bd9be1b91e04737c3ff3d52e
Leahhh <leahhh233@gmail.com>
2019-06-04 09:41:53 -0400

    Add files via upload

--- Database/script/Insert/2019Q1/2019Q1_EmploymentAlltime.sql
+++ Database/script/Insert/2019Q1/2019Q1_EmploymentAlltime.sql
@@ -15,7 +15,7 @@ s12.employmentstatus as employmentstatus_2018Q1, s12.employmentstartdate as empl
 s13.employmentstatus as employmentstatus_2018Q2, s13.employmentstartdate as employmentstartdate_2018Q2, 
 s14.employmentstatus as employmentstatus_2018Q3, s14.employmentstartdate as employmentstartdate_2018Q3, 
 s15.employmentstatus as employmentstatus_2018Q4, s15.employmentstartdate as employmentstartdate_2018Q4,
-s16.employmentstatus as employmentstatus_2019Q1, s15.employmentstartdate as employmentstartdate_2019Q1
+s16.employmentstatus as employmentstatus_2019Q1, s16.employmentstartdate as employmentstartdate_2019Q1
 from 
 (select clientid, quarter, date, baseline_baseline, employmentstatus, employmentstartdate from employment_all
 where baseline_baseline is true)s1
@@ -83,6 +83,24 @@ on s16.clientid = s1.clientid;
 create view employment_alltime_view as 
 select clientid, 
 case
+when employmentstatus_2019q1 is not null then  '2019q1'
+when employmentstatus_2018q4 is not null then  '2018q4'
+when employmentstatus_2018q3 is not null then  '2018q3'
+when employmentstatus_2018q2 is not null then  '2018q2'
+when employmentstatus_2018q1 is not null then  '2018q1'
+when employmentstatus_2017q4 is not null then  '2017q4'
+when employmentstatus_2017q3 is not null then  '2017q3'
+when employmentstatus_2017q2 is not null then  '2017q2'
+when employmentstatus_2017q1 is not null then  '2017q1'
+when employmentstatus_2016q4 is not null then  '2016q4'
+when employmentstatus_2016q3 is not null then  '2016q3'
+when employmentstatus_2016q2 is not null then  '2016q2'
+when employmentstatus_2016q1 is not null then  '2016q1'
+when employmentstatus_2015q4 is not null then  '2015q4'
+when employmentstatus_2015q3 is not null then  '2015q3'
+else 'baseline'
+end as emp_quarter,
+case
 when employmentstatus_2019q1 is not null then  employmentstatus_2019q1
 when employmentstatus_2018q4 is not null then  employmentstatus_2018q4
 when employmentstatus_2018q3 is not null then  employmentstatus_2018q3
@@ -122,26 +140,27 @@ from employment_for_cal
 order by clientid asc;
 
 create view employment_month as 
-select clientid, emp, emp_date,  date_part('year', age(now(),emp_date))*12+date_part('month', age(now(),emp_date)) as months
+select clientid, emp_quarter, emp,  emp_date,  date_part('year', age(now(),emp_date))*12+date_part('month', age(now(),emp_date)) as months
 from employment_alltime_view;
 
 																								  
-create table employment_alltime_2019Q1(
+create table employment_alltime_2019Q1_v2(
 clientid varchar(50) primary key,
+emp_quarter varchar(50),
 emp_recent varchar(50),
 emp_date timestamp,
 months int,
 monthsbin text);
 																								  
-insert into 	employment_alltime_2019Q1(																							  
-select clientid, emp, emp_date, months, 
+insert into 	employment_alltime_2019Q1_v2(																							  
+select clientid, emp_quarter, emp,emp_date, months, 
 case 
 when months >=0 and months <3 then 'Less than 3 months'	
 when months >=3 and months <6 then 'More than 3 months but less than 6 months'																								  
 when months >=6 and months <9 then 'More than 6 months but less than 9 months'
 when months >=9 and months <12 then 'More than 9 months but less than 1 year'
-when months >=12 and months <18 then 'More than 1 year but less than 1.5 year' 
-when months >=18 and months <24 then 'More than 1.5 year but less than 2 year' 
+when months >=12 and months <18 then 'More than 1 year but less than 1.5 years' 
+when months >=18 and months <24 then 'More than 1.5 year but less than 2 years' 
 when months >=24 then 'More than 2 years' 						  
 end	as monthsbin																							  
 from employment_month);
