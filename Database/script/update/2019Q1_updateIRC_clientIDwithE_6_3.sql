f5f8cf2ed39b78123681610ee048800ac8970032
Leahhh <leahhh233@gmail.com>
2019-06-06 17:32:12 -0400

    Add files via upload

--- Database/script/update/2019Q1_updateIRC_clientIDwithE_6_3.sql
+++ Database/script/update/2019Q1_updateIRC_clientIDwithE_6_3.sql
@@ -11,5 +11,11 @@ end
 where clientid in ('1.66822E+11','1.66166E+11');
 
 
+update employment_alltime_2019q1 set clientid = case 
+when clientid = '1.66822E+11' then '166822050356'
+when clientid = '1.66166E+11' then '166166347019'
+end
+where clientid in ('1.66822E+11','1.66166E+11');
+
 select * from records where clientid in ('166822050356','166166347019');
 select * from demographic where clientid in ('166822050356','166166347019');
