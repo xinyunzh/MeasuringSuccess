80df2e318c9b554403acb320f9eab18032930070
Leahhh <leahhh233@gmail.com>
2019-07-09 15:49:35 -0400

    Add files via upload
    update demographic table with residential and tanf column

---
+++ Database/script/update/Update_residential_tanf_7_9.sql
@@ -0,0 +1,10 @@
+ALTER TABLE cft.public.demographic
+add column residential varchar(20),
+add column tanf varchar(20);
+
+
+ALTER TABLE cft.newcohort.demographic
+add column residential varchar(20),
+add column tanf varchar(20);
+
+
